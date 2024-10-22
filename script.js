c89f571586d3159edaea8febbc03e365ed21e831
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-22 15:44:13 +0300

    pages done and first variation of functions done

---
+++ script.js
@@ -0,0 +1,19 @@
+function Player(name,){
+    this.name = name;
+    this.turn = true;
+    
+    this.changeTurn = function(){
+        this.turn = !this.turn
+    }
+}
+
+
+let gameBoard = {
+    board:[9],
+     
+    
+}
+
+function playGame(board,player1,player2){
+
+}
