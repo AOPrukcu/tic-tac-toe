987bbf96b9b9ad5ba8532a547366d5c43ce001f0
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-22 17:33:16 +0300

    before branching almost done

--- script.js
+++ script.js
@@ -1,80 +1,77 @@
-function Player(name,){
-    this.name = name;
-    this.turn = false;
-    
-    this.changeTurn = function(){
-        this.turn = !this.turn
-    }
+let Player(name) {
+  this.name = name;
+  this.playerSymbol = ""
+  this.winStreak = 0;
+  
 
- 
-    put = function(choosenIndex){
-        if(this.board[choosenIndex] ==="undefined"){
-            (this.turn ==true) ?
-            this.board[choosenIndex] =gameBoard.o :  this.board[choosenIndex] =gameBoard.x;
-        }
-       
+  put = function (choosenIndex) {
+    if (this.board[choosenIndex] === "undefined") {
+        this.board[choosenIndex] = this.playerSymbol 
     }
-   
+  };
 }
-
 
 let gameBoard = {
-    board:[9],
-    x : "x",
-    o :"o",
+  board: [9],
+  turn: true ,
+  currentPlayer : true,
+  addTurn : ()=>++this.turn,
+  changeTurn : function () {
+    turn = !turn
+  };
+};
+
+function playGame(player1, player2) {
+  rand = Math.random() * 10 ;
+  if(rand > 5) {
+    player1.playerSymbol= "o";
+    player2.playerSymbol= "x";
+   
+  }else {
+    player1.playerSymbol= "x";
+    player2.playerSymbol= "o";
+  }
+   ;
+  for (let index = 0; index < 9; index++) {
     
     
-    
+  }
 }
-
-function playGame(player1,player2){
-    rand = Math.random() *10
-    (rand > 5)? player1.changeTurn() : player2.changeTurn(); 
-    let gameOn = true;
-    while(gameOn){
-        if(player1.turn ==true){
-
-        }
+squares = document.querySelectorAll(".square");
+squares.forEach((element) => {
+  element.addEventListener("click", (player) =>{
+    if(element.textContent==""){
+        let index = (+element.id ) - 1
+    player.put(index);
+    element.textContent = player.playerSymbol   
     }
-
-}
-squares = document.querySelectorAll(".square")
-squares.forEach(element => {
-    element.addEventListener("click",(player) =>
-    player.put(+element.id))
+    board.addTurn()
+    checkWinner(board);
     
+    
+} 
+  );
 });
-function playBoard(player){
-    player.
-}
+
 
 function checkWinner(board) {
-    const winCombinations = [
-        [0, 1, 2], 
-        [3, 4, 5], 
-        [6, 7, 8],
-        [0, 3, 6], 
-        [1, 4, 7], 
-        [2, 5, 8],
-        [0, 4, 8], 
-        [2, 4, 6]  
-    ];
+  const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    for (const combination of winCombinations) {
-        const [a, b, c] = combination;
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            return board[a]; 
-        }
+  for (const combination of winCombinations) {
+    const [a, b, c] = combination;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
     }
+  }
 
-    return null;
-}
-
-const board = ['x', 'o', 'x', 'x', 'x', 'o', 'o', 'x', 'x'];
-const winner = checkWinner(board);
-
-if (winner) {
-    console.log(`Kazanan: ${winner}`);
-} else {
-    console.log("Kazanan yok");
+  return null;
 }
